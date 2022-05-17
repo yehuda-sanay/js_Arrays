3ed262b3a2b1546f4378abc37a524e2aaec207a8
yehuda-sanay <yehudasanay1@gmail.com>
2022-05-17 22:53:33 +0300

    17.05.22

---
+++ main.js
@@ -0,0 +1,33 @@
+// var num=[5,3,7,8,11];
+// var number=0;
+// for(var i=0; i<5;i++){
+//     number+=num[i];
+// }
+// console.log(number);
+// console.log(number/5);
+
+// var names=["yehuda","sara","may","elinor","yahali"];
+// for(var i=0;i<names.length;i++){
+//     console.log(names[i]);
+// }
+
+
+// לבדוק למה זה לא עובד
+// var number= numbers[j]
+// function sumnumber(num,number){
+//     for(var i=0; i<number.length;i++)
+//         if(number[j]==num){
+//         console.log(true);
+//     }
+// }
+// sumnumber(5,5)
+var num = [n]
+function evennumbers(n){
+    for(var i=0; i<num.length;i++){
+        if(num[n]%2==0){
+            console.log(num[n]);
+        }
+    }
+
+}
+evennumbers(20)
